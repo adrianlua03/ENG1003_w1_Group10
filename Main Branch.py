fea16cf33b1b822bd00b6df8af664ded4fa8de93
Squeryfhk <76478835+Squeryfhk@users.noreply.github.com>
2021-09-17 13:19:11 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -6,4 +6,14 @@ elif start == "2":
 elif start == "3":
     #Jershon's function
 elif start == "4":
-    #Johnny's function
+   side = int(input("Please Enter the Sides of a Square  : "))
+    i = 0
+    print("Square Size: ", side)
+
+    while(i < side):
+    j = 0
+    while(j < side):      
+        j = j + 1
+        print('[]', end = '  ')
+    i = i + 1
+    print('')
