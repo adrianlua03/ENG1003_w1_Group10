29951774ebeb11e2f20dea26781b582aea380d30
Adrian Shalom Lua <90884505+adrianlua03@users.noreply.github.com>
2021-09-17 12:23:25 +0800

    Update Group 10 code.py

--- Group 10 code.py
+++ Group 10 code.py
@@ -1,6 +1,6 @@
 start = input("Enter the function to be executed: ")
 if start == "2":
-    #Jungwook's function
+    #Jay's function
 elif start == "3":
     #Jershon's function
 elif start == "4":
