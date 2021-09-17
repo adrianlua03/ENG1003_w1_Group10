992fbc8a466e5dab51f7ba4361b77ebd38736c51
Jershon Ainsleigh Ang <70652194+JershonA@users.noreply.github.com>
2021-09-17 12:57:32 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -3,10 +3,13 @@ if start == "1":
     x = int(input("Height:"))
 
     print("xxx\n"*x)
-
+elif start == "2":
+    #Jay's function
 elif start == "3":
     x = int(input("List Length:"))
 
     while x>0:
         print(x)
         x-=1
+elif start == "4":
+    #Johnny's function
