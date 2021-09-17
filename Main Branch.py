a2f20aa45a826b89352365ca8d0554b554f760cf
JayC03 <90884303+JayC03@users.noreply.github.com>
2021-09-17 13:20:10 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -2,8 +2,10 @@ start = input("Enter the function to be executed: ")
 if start == "1":
     #Function 1
 elif start == "2":
-    #Jay's function
-elif start == "3":
+    a = int(input("First Number: "))
+    b = int(input("Second Number: "))
+    s = a + b
+    print("The sum is: "), selif start == "3":
     #Jershon's function
 elif start == "4":
     #Johnny's function
