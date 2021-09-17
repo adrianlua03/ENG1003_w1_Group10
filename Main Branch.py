dd78cb22c857cd8fa6b321dc8b04d2190d6ef03f
Jershon Ainsleigh Ang <70652194+JershonA@users.noreply.github.com>
2021-09-17 12:47:22 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -1,13 +1,12 @@
 start = input("Enter the function to be executed: ")
 if start == "1":
-    #Function 1
-elif start == "2":
-    #Jay's function
+    x = int(input("Height:"))
+
+    print("xxx\n"*x)
+
 elif start == "3":
     x = int(input("List Length:"))
 
     while x>0:
         print(x)
         x-=1
-elif start == "4":
-    #Johnny's function
