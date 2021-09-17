76cf141e143f305d22487824c2bb1a1a3fe641a7
Adrian Shalom Lua <90884505+adrianlua03@users.noreply.github.com>
2021-09-17 12:21:52 +0800

    Add files via upload

---
+++ Group 10 code.py
@@ -0,0 +1,7 @@
+start = input("Enter the function to be executed: ")
+if start == "2":
+    #Jungwook's function
+elif start == "3":
+    #Jershon's function
+elif start == "4":
+    #Johnny's function
