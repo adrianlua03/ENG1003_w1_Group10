c79696b4bd88c018b87d0640ef7c59b16f19ac43
Adrian Shalom Lua <90884505+adrianlua03@users.noreply.github.com>
2021-09-20 13:29:07 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -6,20 +6,17 @@ elif start == "2":
 elif start == "3":
     
 elif start == "4":
-
-    
-   side = int(input("Please Enter the Sides of a Square  : "))
+    side = int(input("Please Enter the Sides of a Square  : "))
     i = 0
     print("Square Size: ", side)
 
     while(i < side):
-    j = 0
+        j = 0
     while(j < side):      
         j = j + 1
         print('[]', end = '  ')
     i = i + 1
     print('')
 
-    
 # LT visited here :)
 
