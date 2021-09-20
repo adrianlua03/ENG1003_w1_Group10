43b5010ae1c1b62e05f73ea88c2464af458437e4
Adrian Shalom Lua <90884505+adrianlua03@users.noreply.github.com>
2021-09-20 13:16:45 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -11,33 +11,7 @@ elif start == "3":
 elif start == "4":
     #Johnny's function
 
-start = input("Enter the function to be executed: ")
-if start == "1":
-    x = int(input("Height:"))
 
-    print("xxx\n"*x)
-elif start == "2":
-    #Jay's function
-elif start == "3":
-    x = int(input("List Length:"))
-
-    while x>0:
-        print(x)
-        x-=1
-elif start == "4":
-
-    
-   side = int(input("Please Enter the Sides of a Square  : "))
-    i = 0
-    print("Square Size: ", side)
-
-    while(i < side):
-    j = 0
-    while(j < side):      
-        j = j + 1
-        print('[]', end = '  ')
-    i = i + 1
-    print('')
 
     
 # LT visited here :)
