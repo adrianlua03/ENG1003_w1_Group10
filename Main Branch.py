96a1ace6276097ebf0e4a0997479f49244c1bc4c
Adrian Shalom Lua <90884505+adrianlua03@users.noreply.github.com>
2021-09-20 13:20:39 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -1,15 +1,15 @@
 start = input("Enter the function to be executed: ")
 if start == "1":
-    #Function 1
+    
 elif start == "2":
     a = int(input("First Number: "))
     b = int(input("Second Number: "))
     s = a + b
     print("The sum is: ", s) 
 elif start == "3":
-    #Jershon's function
+    
 elif start == "4":
-    #Johnny's function
+    
 
 
 
