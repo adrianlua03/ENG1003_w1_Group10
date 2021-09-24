d1874834e97f28f353f3c0fc20c3e7e5c6c43c8b
Jershon Ainsleigh Ang <70652194+JershonA@users.noreply.github.com>
2021-09-24 12:13:40 +0800

    Update Main Branch.py

--- Main Branch.py
+++ Main Branch.py
@@ -19,14 +19,39 @@ elif start == "4":
   i = 0
   print("Square Size: ", side)
 
-    while(i < side):
-        j = 0
-    while(j < side):      
-        j = j + 1
-        print('[]', end = '  ')
-    i = i + 1
-    print('')
-    
+  while(i < side):
+      j = 0
+      while(j < side):      
+          j = j + 1
+          print('[]', end = '  ')
+      i = i + 1
+      print('')
+
+elif start == "5":
+  import random
+
+  r = random.randint(50,200)
+  x = random.randint(1,r)
+  y=0
+  z=1
+  print('The range for this round is 1 -', r)
+  t = 0
+
+  while not y == x:
+      y = int(input("Make a guess:"))
+      if y>x:
+          r=y
+          print('The range is now',z,'-',r,'\n')
+          t+=1
+      
+      elif y<x:
+          z=y
+          print('The range is now',z,'-',r,'\n')
+          t+=1
+
+      else:
+          t+=1
+          print('BINGO! You took',t,'attempts to reach the answer!','\n')
 # LT visited here :)
 
 
